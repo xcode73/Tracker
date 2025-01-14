20e3f5151731947a50f971f83af3251ecc146c5d
Nikolai Eremenko <nikolai.dev@icloud.com>
2025-01-14 14:59:54 +0400

    refactor: change forders structure

--- Tracker/Application/SceneDelegate.swift
+++ Tracker/Application/SceneDelegate.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
@@ -19,14 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) { }
-
-    func sceneDidBecomeActive(_ scene: UIScene) { }
-
-    func sceneWillResignActive(_ scene: UIScene) { }
-
-    func sceneWillEnterForeground(_ scene: UIScene) { }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         try? (UIApplication.shared.delegate as! AppDelegate).trackerDataStore.saveContext()
